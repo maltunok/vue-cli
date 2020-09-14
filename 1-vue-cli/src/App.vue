5d6a0a92376a627cf21768acdec5b9d3d29f596b
Mustafa Altunok <m_altunok@hotmail.com>
2020-09-14 23:28:05 +0300

    products

--- 1-vue-cli/src/App.vue
+++ 1-vue-cli/src/App.vue
@@ -1,7 +1,16 @@
 <template>
-<div>
+<div class="container">
+  <div class="cart">
+    <h3>sepet</h3>
+    <ul>
+      <li v-for="item in cartItems" :key="item.id">{{item.title}}</li>
+    </ul>
+    <p v-if ="cartItems.length==0">Sepetiniz boştur...Alışverişe başlayabilirsiniz...</p>
+  </div>
+  <hr>
   <!--<myComp></myComp>-->
-  <Product/>
+  <Product @addcart="cartItems.push($event)" :product ="product" v-for="product in productList" :key="product.id"/>
+
 </div>
 </template>
 <script>
@@ -11,9 +20,45 @@ export default{
 components:{
   //myComp: MainContainer
   Product
+},
+data(){
+  return{
+    productList: [
+      {
+        id:1,
+        title:"McBookPro",
+        price:100,
+        count:50,
+        description:"ssss"
+      },
+      {
+        id:2,
+        title:"iPhone",
+        price:50,
+        count:100,
+        description:"dddd"
+      },
+      {
+        id:3,
+        title:"keyboard",
+        price:10,
+        count:1000,
+        description:"ffff"
+      }
+    ],
+    cartItems :[]
+  }
 }
 }
 </script>
 <style>
-
+.container{
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+}
+.cart{
+  margin-bottom: 50px;
+}
 </style>
--- 1-vue-cli/src/components/Product.vue
+++ 1-vue-cli/src/components/Product.vue
@@ -1,16 +1,34 @@
 <template>
     <div class="product">
-        <strong>Product Name</strong>
-        <p>Explainations</p>
-        <strong>Price:20 | Count:10</strong>
+        <strong>{{product.title}}</strong>
+        <p>{{product.description}}</p>
+        <strong>Price:{{product.price}} | Count: {{product.count}}</strong>
+        <br>
+        <br>
+        <button @click="addcart(product)">sepete ekle</button>
      
     </div>
 </template>
 <script>
 export default {
-    
+    props: ["product"],
+    methods:{
+        addcart(product){
+         this.$emit("addcart", product)
+        }
+    }
 }
 </script>
 <style>
+.product {
+    border:2px dashed #666; 
+    padding: 10px 10px;
+    width:300px;
+    margin-bottom: 5px;
+}   
+.product:hover{
+    border:2px solid #666;
+    transition: all .5;
+}
 
 </style>
